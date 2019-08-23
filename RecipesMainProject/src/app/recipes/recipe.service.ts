3d92fe5f67bd9fd1b4da2b0ceab230ac864428d6
David Goodenough <RomainBenard@cerp-rouen.fr>
2019-08-23 16:05:02 +0200

    Main project : end part section 10 services

--- RecipesMainProject/src/app/recipes/recipe.service.ts
+++ RecipesMainProject/src/app/recipes/recipe.service.ts
@@ -1,5 +1,7 @@
 import { Injectable, EventEmitter } from '@angular/core';
 import { Recipe } from './recipes.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
 @Injectable({
   providedIn: 'root'
@@ -9,15 +11,28 @@ export class RecipeService {
 
   private recipes: Recipe[] = [
     new Recipe('A test recipe', 'This is simply a test',
-    'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg'),
+    'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg'
+    , [
+      new Ingredient('Meat', 2),
+      new Ingredient('Butter', 1)
+    ]),
     new Recipe('A second test recipe', 'My second recipe',
-    'https://upload.wikimedia.org/wikipedia/commons/2/29/Aunt_Edith%27s_Banana_Bread_Recipe.jpg')
+    'https://upload.wikimedia.org/wikipedia/commons/2/29/Aunt_Edith%27s_Banana_Bread_Recipe.jpg'
+    , [
+      new Ingredient('Milk', 2),
+      new Ingredient('Rice', 1),
+      new Ingredient('Apple', 5)
+    ])
   ];
 
-  constructor() { }
+  constructor(private slService: ShoppingListService) { }
 
   getRecipes() {
     // Renvoie une copie du tableau de données!
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
   }
 }
