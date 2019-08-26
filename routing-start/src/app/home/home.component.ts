aaec931c1f3c0ebfdb1121f603913b07b684f760
David Goodenough <RomainBenard@cerp-rouen.fr>
2019-08-26 09:56:10 +0200

    Query parameters and fragments

--- routing-start/src/app/home/home.component.ts
+++ routing-start/src/app/home/home.component.ts
@@ -17,4 +17,8 @@ export class HomeComponent implements OnInit {
     // Executing code here before navigation
     this.router.navigate(['/servers']);
   }
+
+  onLoadServer(id: number) {
+    this.router.navigate(['/servers', id, 'edit'], {queryParams: {allowEdit: '1'}, fragment: 'loading'});
+  }
 }
