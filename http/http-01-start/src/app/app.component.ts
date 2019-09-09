4416bfbdca72704ffaa86a3de0fa0315bcd141d0
David Goodenough <r.benard87@gmail.com>
2019-09-09 09:49:27 +0200

    Show loading indicator

--- http/http-01-start/src/app/app.component.ts
+++ http/http-01-start/src/app/app.component.ts
@@ -10,6 +10,7 @@ import { Post } from './post.model';
 })
 export class AppComponent implements OnInit {
   loadedPosts: Post[] = [];
+  isFetching = false;
 
   constructor(private http: HttpClient) {}
 
@@ -39,6 +40,7 @@ export class AppComponent implements OnInit {
   }
 
   private fetchPosts() {
+    this.isFetching = true;
     this.http
       .get<{[key: string]: Post}>('https://rbeangularudemy.firebaseio.com/posts.json')
       .pipe(
@@ -52,6 +54,7 @@ export class AppComponent implements OnInit {
         return postsArray;
       }))
       .subscribe(posts => {
+      this.isFetching = false;
       this.loadedPosts = posts;
     });
   }
