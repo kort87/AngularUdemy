4d23c8c79a5f529710c2d8e37ddba4a3f8d73a39
David Goodenough <r.benard87@gmail.com>
2019-09-12 10:07:46 +0200

    End section 19 main project: http

--- RecipesMainProject/src/app/header/header.component.ts
+++ RecipesMainProject/src/app/header/header.component.ts
@@ -17,6 +17,6 @@ export class HeaderComponent {
   }
 
   onFetchData() {
-    this.dsService.fetchRecipes();
+    this.dsService.fetchRecipes().subscribe();
   }
 }
--- RecipesMainProject/src/app/shared/data-storage.service.ts
+++ RecipesMainProject/src/app/shared/data-storage.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipes.model';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 
 
 @Injectable({
@@ -23,16 +23,17 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    this.http.get<Recipe[]>('https://angularudemyrbemainbackend.firebaseio.com/recipes.json')
-      .pipe(map(recipes => {
-        return recipes.map(recipe => {
-          // Initialise les ingéredients à un tableau vide pour les recettes récupérées n'en ayant pas.
-          return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
-        });
-      }))
-      .subscribe(recipes => {
-        console.log(recipes);
-        this.recipeService.setRecipes(recipes);
-      });
+    return this.http.get<Recipe[]>('https://angularudemyrbemainbackend.firebaseio.com/recipes.json')
+      .pipe(
+        map(recipes => {
+          return recipes.map(recipe => {
+            // Initialise les ingéredients à un tableau vide pour les recettes récupérées n'en ayant pas.
+            return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
+          });
+        }),
+        tap(recipes => {
+          this.recipeService.setRecipes(recipes as Recipe[]);
+        })
+      );
   }
 }
