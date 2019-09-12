71be60a2a974719c337a11455769ebf9ff36418f
David Goodenough <r.benard87@gmail.com>
2019-09-12 11:29:39 +0200

    Sending signup request

--- RecipesMainProject/src/app/auth/auth.service.ts
+++ RecipesMainProject/src/app/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { AuthResponseData } from './authresponsedata.model';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,7 @@ export class AuthService {
   constructor(private http: HttpClient) { }
 
   signup(email: string, password: string) {
-    return this.http.post(
+    return this.http.post<AuthResponseData>(
       'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDFnuNd3PZlNLkakZFmRZj5HDxbXYf79zg',
       {
         email: email,
