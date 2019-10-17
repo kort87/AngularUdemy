9b74e2d37a448681ebd023383bd9a703107362aa
David Goodenough <RomainBenard@cerp-rouen.fr>
2019-10-17 17:12:34 +0200

    Autologin

--- RecipesMainProject/src/app/auth/auth.service.ts
+++ RecipesMainProject/src/app/auth/auth.service.ts
@@ -37,15 +37,33 @@ export class AuthService {
       }));
   }
 
+  autoLogin() {
+    const userData: {
+      email: string;
+      id: string;
+      _token: string;
+      _tokenExpirationDate: string
+    } = JSON.parse(localStorage.getItem('userData'));
+    if (!userData) {
+      return;
+    }
+    const loadedUser = new User(userData.email, userData.id, userData._token, new Date(userData._tokenExpirationDate));
+    if (loadedUser.token) {
+      this.userSubject.next(loadedUser);
+    }
+  }
+
   logout() {
     this.userSubject.next(null);
     this.router.navigate(['/auth']);
+    localStorage.removeItem('userData');
   }
 
   private handleAuthentication(email: string, userId: string, token: string, expiresIn: number) {
     const expirationDate = new Date(new Date().getTime() + +expiresIn * 1000);
     const user = new User(email, userId, token, expirationDate);
     this.userSubject.next(user);
+    localStorage.setItem('userData', JSON.stringify(user));
   }
 
   private handleError(errorRes: HttpErrorResponse) {
